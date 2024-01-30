c18811ff69fa9069ceb59e9da51bef66f2af4c75
Thomastavrou <thomastavrou@gmail.com>
2024-01-30 23:10:54 +0200

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-// config.js
-module.exports = {
-  firebaseServiceAccountPath: './rabbitloan-app-firebase-adminsdk-439mr-fdaa7bf1ce.json',
-  openaiApiKey: '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',
-  firebaseStorageBucket: 'rabbitloan-app.appspot.com',
-};
